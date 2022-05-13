b42cf03b5afa49eb5f1b2787705e1d9f34ac6dd3
Nikola Ilic <nikola.ilic12358@gmail.com>
2022-05-13 20:31:39 +0200

    Predavanja 13.05. Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,10 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git add");
+        System.out.println("git commit -m");
+        System.out.println("git push");
+
+    }
+}
