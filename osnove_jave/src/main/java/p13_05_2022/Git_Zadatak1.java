fb6fbdd8be79d90cea7423255c770b6676e46656
Nikola Ilic <nikola.ilic12358@gmail.com>
2022-05-13 21:04:33 +0200

    Ispravke

--- osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -2,6 +2,8 @@ package p13_05_2022;
 
 public class Git_Zadatak1 {
     public static void main(String[] args) {
+        System.out.println("git clone");
+        System.out.println("git status");
         System.out.println("git add");
         System.out.println("git commit -m");
         System.out.println("git push");
